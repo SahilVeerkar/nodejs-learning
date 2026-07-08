8f1b92ad1b4e04dcea38a322373f2c5e18ccbe5c
sahilVeerkar <veerkarsahil@gmail.com>
2026-07-08 16:51:08 +0530

    nodemon intall

---
+++ 4-npm-and-Tools/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+  console.log(req);
+});
+const PORT=3002;
+server.listen(PORT,()=>{
+  console.log(`server running on address http://localhost:${PORT}`);
+})
