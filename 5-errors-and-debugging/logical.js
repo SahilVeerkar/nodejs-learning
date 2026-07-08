4ba6a2c2d3b6824446977e33ac2201f56ca1f99e
sahilVeerkar <veerkarsahil@gmail.com>
2026-07-08 23:09:54 +0530

    error and debugging

---
+++ 5-errors-and-debugging/logical.js
@@ -0,0 +1,7 @@
+const logical=()=>{
+let x=5; 
+if(num=10){
+  console.log(num);
+}
+}
+module.exports=logical;
---
+++ 5-errors-and-debugging/syntax.js
@@ -0,0 +1,4 @@
+const testingSyntax=()=>{
+console.log("i am inside testing syntax");
+}
+module.exports=testingSyntax;
